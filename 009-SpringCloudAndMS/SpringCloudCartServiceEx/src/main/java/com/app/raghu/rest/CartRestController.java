49510b289583728438e05c8171ce9b7cd658ebc7
RAGHU SIR <javabyraghu@gmail.com>
2023-03-14 06:37:02 +0530

    Config Server with Refresh Scope

--- 009-SpringCloudAndMS/SpringCloudCartServiceEx/src/main/java/com/app/raghu/rest/CartRestController.java
+++ 009-SpringCloudAndMS/SpringCloudCartServiceEx/src/main/java/com/app/raghu/rest/CartRestController.java
@@ -1,6 +1,7 @@
 package com.app.raghu.rest;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,14 +12,15 @@ import com.app.raghu.bean.CartInfo;
 
 @RestController
 @RequestMapping("/cart")
+@RefreshScope
 public class CartRestController {
 	
-	@Value("${server.port}")
-	private String port;
+	@Value("${my.app.title}")
+	private String title;
 
 	@GetMapping("/message")
 	public ResponseEntity<String> showMsgFromCart() {
-		return ResponseEntity.ok(":: FROM CART SERVICE :: " + port);
+		return ResponseEntity.ok(":: FROM CART SERVICE :: " + title);
 	}
 	
 	@GetMapping("/info/{code}")
